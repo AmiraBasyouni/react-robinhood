746ae0f36483c3e38620938cd3ada02c61ff28b0
AmiraBasyouni <basyouniamira1@gmail.com>
2021-10-17 15:51:56 -0700

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import Logo from './Logo.svg';
+import React from 'react';
 import './App.css';
 import Header from './Header';
 import Newsfeed from './Newsfeed';
 import Stats from './Stats';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      {/* Header */}
       <div className="app__header">
         <Header />
       </div>
       <div className="app__body">
         <div className="app__container">
           <Newsfeed />
-          {/*stats*/}
           <Stats />
         </div>
       </div>
-
-      {/* Body */}
     </div>
   );
 }
-
-export default App;
