3abf16e7e4422b1d9b81d6549b6ac033ec84754b
AmiraBasyouni <basyouniamira1@gmail.com>
2021-10-17 16:19:43 -0700

    WorkAroundchartjsXDisplayError

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -2,15 +2,30 @@ import React from 'react';
 import { Line } from 'react-chartjs-2';
 
 function LineGraph() {
-  const data = [
-    { x: 10, y: 20 },
-    { x: 15, y: 10 },
-    { x: 12, y: 4 },
-    { x: 5, y: 5 },
-  ];
+  // const data = [
+  //   { x: 10, y: 20 },
+  //   { x: 15, y: 10 },
+  //   { x: 12, y: 4 },
+  //   { x: 5, y: 5 },
+  // ];
+  const oLinegraphData = {
+    labels: [10, 12, 15],
+    datasets: [
+      {
+        backgroundColor: 'rgba(75,192,192,0.2)',
+        borderColor: 'rgba(75,192,192,1)',
+        data: [20, 4, 10],
+        label: 'First dataset',
+        fill: true,
+        type: 'line',
+      },
+    ],
+    labels: [10, 12, 15],
+  };
   return (
     <div className="linegraph">
-      <Line
+      <Line data={oLinegraphData} />
+      {/* <Line
         data={{
           datasets: [
             {
@@ -46,7 +61,7 @@ function LineGraph() {
             ],
           },
         }}
-      />
+      /> */}
     </div>
   );
 }
